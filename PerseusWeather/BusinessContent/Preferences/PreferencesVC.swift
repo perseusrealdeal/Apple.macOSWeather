14d1935583eb4b5bacfa09f2dcd840ad8129d848
Mikhail <mzhigulin@gmail.com>
2023-09-07 12:44:57 +0700

    UPDATED: license notices

--- PerseusWeather/BusinessContent/Preferences/PreferencesVC.swift
+++ PerseusWeather/BusinessContent/Preferences/PreferencesVC.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Mikhail Zhigulin in 7531.
 //
-//  Copyright © 7531 Mikhail Zhigulin of Novosibirsk.
-//  Copyright © PerseusRealDeal.
+//  Copyright © 7531 - 7532 Mikhail Zhigulin of Novosibirsk
+//  Copyright © 7531 - 7532 PerseusRealDeal
 //
-//  Licensed under the special license. See LICENSE file.
-//  All rights reserved.
+//  The year starts from the creation of the world according to a Slavic calendar.
+//  September, the 1st of Slavic year.
+//
+//  See LICENSE for details. All rights reserved.
 //
 
 import Cocoa
