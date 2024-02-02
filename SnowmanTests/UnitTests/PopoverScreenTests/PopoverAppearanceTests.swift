c665812a3868b69c85109c70fac50c388ce0f4db
Mikhail <mzhigulin@gmail.com>
2024-02-02 09:58:08 +0700

    UPDATED: copyright details

--- SnowmanTests/UnitTests/PopoverScreenTests/PopoverAppearanceTests.swift
+++ SnowmanTests/UnitTests/PopoverScreenTests/PopoverAppearanceTests.swift
@@ -7,8 +7,8 @@
 //  Copyright © 7532 Mikhail Zhigulin of Novosibirsk
 //  Copyright © 7532 PerseusRealDeal
 //
-//  The year starts from the creation of the world according to a Slavic calendar.
-//  September, the 1st of Slavic year.
+//  The year starts from the creation of the world in the Star temple
+//  according to a Slavic calendar. September, the 1st of Slavic year.
 //
 //  See LICENSE for details. All rights reserved.
 //
